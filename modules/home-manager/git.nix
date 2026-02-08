e2a2bd40ea04bccdd7b94b90bb79752c8f89e91e
Gandalf-Le-Dev <matproz.gaming@gmail.com>
2026-02-08 13:01:49 +0100

    Fix git config deprecation warnings
    - Updated to use programs.git.settings structure
    - userName/userEmail moved to settings.user.name/email
    - extraConfig merged into settings

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,30 +3,39 @@
 {
   programs.git = {
     enable = true;
-    userName = "Gandalf-Le-Dev";
-    userEmail = "matproz.gaming@gmail.com";
 
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Gandalf-Le-Dev";
+        email = "matproz.gaming@gmail.com";
+      };
+
       core = {
         autocrlf = "input";
         editor = "code --wait";
       };
+
       merge = {
         tool = "vscode";
       };
+
       "mergetool \"vscode\"" = {
         cmd = "code --wait ";
       };
+
       diff = {
         tool = "vscode";
       };
+
       "difftool \"vscode\"" = {
         cmd = "code --wait --diff  ";
       };
+
       alias = {
         publish = ''!f() { git push --set-upstream "''${1:-origin}" "$(git current-branch)"; }; f'';
         current-branch = "rev-parse --abbrev-ref HEAD";
       };
+
       "url \"ssh://git@github.com/\"" = {
         insteadOf = "https://github.com/";
       };
